26ddc96f12ab7f311fe2bf58e65ea08a0a7900d4
uttk <uttk8128@gmail.com>
2020-01-28 02:42:42 +0900

    Add module css plugins to ui

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,33 +1,35 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import styles from './index.scss';
 
 const JSONPreview = () => {
   const [json, setJSON] = React.useState({});
-  const [error, setError] = React.useState("");
+  const [error, setError] = React.useState('');
 
   React.useEffect(() => {
-    fetch("/dependencies").then(res => {
+    fetch('/dependencies').then((res) => {
       res
         .json()
         .then(setJSON)
-        .catch(() => setError("情報の取得に失敗しました"));
+        .catch(() => setError('情報の取得に失敗しました'));
     });
   }, []);
 
   return (
     <div>
+      <h2>Json Preview</h2>
       <code>{JSON.stringify(json)}</code>
-      <p style={{ color: "red" }}>{error}</p>
+      <p style={{ color: 'red' }}>{error}</p>
     </div>
   );
 };
 
 const App = () => (
   <>
-    <h1>Hello World!</h1>
+    <h1 className={styles.title}>Hello World!</h1>
 
     <JSONPreview />
   </>
 );
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'));
--- packages/ui/webpack.config.js
+++ packages/ui/webpack.config.js
@@ -1,20 +1,35 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-  entry: path.resolve('src/index.tsx'),
+  entry: path.resolve("src/index.tsx"),
 
   mode: "production",
 
   output: {
-    path: path.resolve(__dirname, './public'),
+    path: path.resolve(__dirname, "./public"),
     publicPath: "/",
-    filename: 'bundle.js',
+    filename: "bundle.js"
   },
 
   module: {
-    rules: [{
-      test: /\.(ts|tsx)$/,
-      use: 'ts-loader',
-    }],
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: "ts-loader"
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        loader: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true
+            }
+          },
+          "sass-loader"
+        ]
+      }
+    ]
   }
-}
+};
