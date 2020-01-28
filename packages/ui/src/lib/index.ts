17289d0320ec8c5eca74e535ffcff056fbe9c291
uttk <uttk8128@gmail.com>
2020-01-28 23:22:44 +0900

    Fix to components of @react-izon/ui

--- packages/ui/src/lib/index.ts
+++ packages/ui/src/lib/index.ts
@@ -1,31 +1,28 @@
 import open from 'open';
 import express from 'express';
-import { Dependency, Dependencies } from '@react-izon/core';
+import { Dependencies } from '@react-izon/core';
 
 const app = express();
 
-export default function startServer(json : Dependencies) {
-  app.use(express.static('public'));
-
-  const jsonRoute = express.Router();
-
-  Object.keys(json).forEach((name) => {
-    jsonRoute.get(`/${name}`, (req, res) => {
-      res.json(json[name]);
-    });
-  });
-
-  app.use('/json', jsonRoute);
-
-  app.listen(9000, () => {
-    console.log('Get starting server!!'); // eslint-disable-line no-console
-  });
-
-  open('http://localhost:9000');
+interface ReactIzonUIOptions {
+  port?: number;
+  isOpen?: boolean;
 }
 
-const dependency = {
-  filePath: 'test.jsx', parent: '', defined: {}, jsx: [],
-} as Dependency;
+export default function startServer(json : Dependencies, options: ReactIzonUIOptions = {}) {
+  const { port = 9000, isOpen = true } = options;
 
-startServer({ Hello: dependency });
+  app.use(express.static('public'));
+
+  app.get('json', (req, res) => {
+    res.json(json);
+  });
+
+  app.listen(port, () => {
+    console.log('Get starting server!!');
+  });
+
+  if (isOpen) {
+    open(`http://localhost:${port}`);
+  }
+}
