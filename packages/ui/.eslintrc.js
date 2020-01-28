20a62f97f71cd1c0adb6b7035462b1c64f44a999
uttk <uttk8128@gmail.com>
2020-01-28 23:13:11 +0900

    Fix to .eslintrc.js of @react-izon/ui

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -15,12 +15,21 @@ module.exports = {
     },
     ecmaVersion: 2018,
     sourceType: "module",
-    project: [ "./tsconfig.json", "./tsconfig.lib.json"]
+    project: ["./tsconfig.json", "./tsconfig.lib.json"]
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".json", ".ts", ".tsx"]
+      }
+    }
   },
   plugins: ["react", "@typescript-eslint"],
   rules: {
+    "import/prefer-default-export": "off",
     "react/jsx-filename-extension": "off",
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": "error"
+    "@typescript-eslint/no-unused-vars": "error",
+    "semi": ["error", "always"],
+    "react/prop-types": "off"
   }
 };
