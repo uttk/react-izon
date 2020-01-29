bcbf3d2a6e688c8bdc5fae996e04d61816ce8816
uttk <uttk8128@gmail.com>
2020-01-30 07:51:13 +0900

    Fix to cli commands

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,29 +1,60 @@
-import {Command, flags} from '@oclif/command'
+import { Command, flags } from "@oclif/command";
+import { DependencyChecker } from "@react-izon/core";
+import { startServer, ServerOptions } from "@react-izon/ui";
 
 class ReactIzon extends Command {
-  static description = 'describe the command here'
+  static description = `
+  Show React Component dependencies in browser`;
+
+  static usage = "ROOT_COMPONENT_FILE_PATH <options>";
+
+  static examples = [
+    "react-izon ./app.jsx",
+    "react-izon ./app.jsx -p 5000",
+    "react-izon ./app.tsx"
+  ]
 
   static flags = {
-    // add --version flag to show CLI version
-    version: flags.version({char: 'v'}),
-    help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
+    version: flags.version({ char: "v" }),
+    help: flags.help({ char: "h" }),
+    open: flags.boolean({ char: "o", default: true }),
+    port: flags.string({ char: "p", default: "9000" })
+  };
+
+  static args = [
+    {
+      name: "ROOT_COMPONENT_FILE_PATH",
+      required: true,
+      description:
+        "File path of the root component of React Components to be analyzed"
+    }
+  ];
+
+  private check(path: string, options: ServerOptions) {
+    const checker = new DependencyChecker(path, depenencies => {
+      startServer(depenencies, options);
+    });
+
+    checker.check();
   }
 
-  static args = [{name: 'file'}]
-
   async run() {
-    const {args, flags} = this.parse(ReactIzon)
+    const { argv, flags } = this.parse(ReactIzon);
+    const path: string = argv[0];
+    const options: ServerOptions = {
+      isOpen: flags.open,
+      port: parseInt(flags.port) || 9000,
+      listenCallback: () => {
+        this.log(`listen to http://localhost:${options.port}`);
+      }
+    };
 
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from .\\src\\index.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    try {
+      this.check(path, options);
+    } catch (error) {
+      this.error(error.message);
     }
   }
 }
 
-export = ReactIzon
+export = ReactIzon;
